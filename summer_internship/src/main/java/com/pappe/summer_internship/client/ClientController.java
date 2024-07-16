693c696c1faa0f5cdd701207b17fb0c0e24cfc18
Chouchenanis <chouchenanis998@gmail.com>
2024-07-16 15:13:16 +0100

    final

--- summer_internship/src/main/java/com/pappe/summer_internship/client/ClientController.java
+++ summer_internship/src/main/java/com/pappe/summer_internship/client/ClientController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/clients")
 public class ClientController {
